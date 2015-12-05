439cdadfde707b1ad1a6d7d5d561319fc7562180
Takao Sekiguchi <takao1080@gmail.com>
2015-12-05 10:42:11 +0900

    リファクタ。クラスを整理しただけ

--- mayoi.cpp
+++ mayoi.cpp
@@ -5,21 +5,21 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-static int P = 0;  // ルート数
-
-class Mayoi {
+class Route {
 private:
   int cur_pos;   // 現在の位置 (Y,A,B,C,Z を 0,1,2,3,4 に対応させる)
   int inv_times; // 反転回数
   int inv_times_limit;  // 反転の上限数 (N)
-//  vector<int> route;  // 経路履歴 (デバッグ向け)
-
+  
   // 進行方向
   enum {
     POS,
     NEG,
   } dir;
-  
+
+//  vector<int> route;  // 経路履歴 (デバッグ向け)
+
+public:
   // 関数の戻り値
   enum {
     FINISH,     // 反転上限内で Y に到達
@@ -27,45 +27,16 @@ private:
     FAIL,       // 反転上限を超えた または Z に到達
   };
 
-public:
   // 初期状態は X から B に来た時点
-  Mayoi() : cur_pos(2), inv_times(0), dir(POS) {
+  Route(int limit) : cur_pos(2), inv_times(0), inv_times_limit(limit), dir(POS) {
 //    route.push_back(cur_pos);
   }
-  /*
-  Mayoi(const Mayoi& rhs) : cur_pos(rhs.cur_pos), inv_times(rhs.inv_times), inv_times_limit(rhs.inv_times_limit), route(rhs.route), dir(rhs.dir) {
+  
+  /*  
+  Route(const Route& rhs) : cur_pos(rhs.cur_pos), inv_times(rhs.inv_times), inv_times_limit(rhs.inv_times_limit), dir(rhs.dir), route(rhs.route) {
   }
   */
-
-  void set_limit(int limit) {inv_times_limit = limit;}
-
-  // 経路探索して P を求める
-  void solve() {
-    // 状態をコピー
-    Mayoi s_in(*this);
-    
-    int result = walk_pos();
-    if (result == FINISH) {
-      P++;
-//      print_route("OK");
-    } else if (result == FAIL) {
-//      print_route("NG");
-    } else {
-      solve();
-    }
-    
-    result = s_in.walk_neg();
-    if (result == FINISH) {
-      P++;
-//      s_in.print_route("OK");
-    } else if (result == FAIL) {
-//      s_in.print_route("NG");
-    } else {
-      s_in.solve();
-    }
-  }
   
-private:
   /*
   void print_route(const char* str) {
     cout << str << " : ";
@@ -111,22 +82,66 @@ private:
     }
     return CONTINUE;
   }
+};
+
+class Solver {
+private:
+  int n_routes;  // 経路数 (P)
   
+public:
+  Solver() {}
+
+  // 経路探索して P を求める
+  int solve(int limit_times) {
+    n_routes = 0;
+    Route r(limit_times);
+    
+    recur_solve(r);
+    return n_routes;
+  }
+
+private:
+  // 再帰による探索
+  void recur_solve(Route& r) {
+    // 状態をコピー
+    Route r_neg(r);
+    
+    int result = r.walk_pos();
+    if (result == Route::FINISH) {
+      n_routes++;
+//      r.print_route("OK");
+    } else if (result == Route::FAIL) {
+//      r.print_route("NG");
+    } else {
+      recur_solve(r);
+    }
+    
+    result = r_neg.walk_neg();
+    if (result == Route::FINISH) {
+      n_routes++;
+//      r_neg.print_route("OK");
+    } else if (result == Route::FAIL) {
+//      r_neg.print_route("NG");
+    } else {
+      recur_solve(r_neg);
+    }
+  }
 };
 
 
-    
+/**
+   マヨイドーロ問題.
+   標準入力から N を入力し、標準出力に P を出力する.
+ */
 int main()
 {
-  Mayoi mayoi;
-
   // 与えられた N に対して P を求める
   int n;
   cin >> n;
-  mayoi.set_limit(n);
 
-  mayoi.solve();
+  Solver solver;
+  int p = solver.solve(n);
 
-  cout << P << endl;
+  cout << p << endl;
 }
   
